797886178c744990c5f380db646904c5085502ba
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2019-04-02 23:13:38 +0100

    filling points coords with 0

--- makehuman/plugins/9_brighter_ai_mhplugin/util/savers.py
+++ makehuman/plugins/9_brighter_ai_mhplugin/util/savers.py
@@ -106,9 +106,12 @@ class AttributeSaver(Saver):
         os.mkdir('{}/{}/vertices'.format(self.save_dir, self.md.get('data_dir')), 0o777)
         os.mkdir('{}/{}/uv_maps'.format(self.save_dir, self.md.get('data_dir')), 0o777)
         self.file = open(os.path.join(self.save_dir, self.file_name), 'w+')
+        points_col_names = ''
+        for i in range(68):
+            points_col_names += ',x_{},y_{}'.format(i, i)
         self.file.write(
             'index,model_uid,image,age,gender,dominant_gender,skin,expression,l_position_x,l_position_y,l_position_z,l_color_r,l_color_g,'
-            'l_color_b,l_specular_r,l_specular_g,l_specular_b,center_x,center_y,center_z,cam_angle_0,cam_angle_1\n')
+            'l_color_b,l_specular_r,l_specular_g,l_specular_b,center_x,center_y,center_z,cam_angle_0,cam_angle_1' + points_col_names + '\n')
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -118,10 +121,12 @@ class AttributeSaver(Saver):
         return int(self.md.get('macrodetails/Gender') >= 0.5)
 
     def save(self):
-        self.file.write('{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n'.
+        points_coords = [0] * 136
+        formatted_str = '{}' + ',{}' * 157 + '\n'
+        self.file.write(formatted_str.
                         format(self.index, self.md.get('model_uid'), self.md.get('image'), self.md.get('real_age'), self.md.get('macrodetails/Gender'), self.get_dominant_gender(),
                                self.md.get('skin'), self.md.get('expression'), self.md.get('l_position_x'), self.md.get('l_position_y'),
                                self.md.get('l_position_z'), self.md.get('l_color_r'), self.md.get('l_color_g'), self.md.get('l_color_b'),
                                self.md.get('l_specular_r'), self.md.get('l_specular_g'), self.md.get('l_specular_b'),
-                               self.md.get('center_x'), self.md.get('center_y'), self.md.get('center_z'), self.md.get('camera_x'), self.md.get('camera_y')))
+                               self.md.get('center_x'), self.md.get('center_y'), self.md.get('center_z'), self.md.get('camera_x'), self.md.get('camera_y'), *points_coords))
         self.index += 1
